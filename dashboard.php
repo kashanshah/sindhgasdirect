5f8e6bf6617a1308c77d9c46aaf62c881bc7963f
Kashan Shah <kashanshah@hotmail.com>
2019-03-24 00:51:26 +0500

    Dashboard graph added

--- dashboard.php
+++ dashboard.php
@@ -79,7 +79,7 @@ if (isset($_POST["mail"]) && $_POST["mail"] == "quickmail") {
     <link rel="stylesheet" href="plugins/morris/morris.css">
     <!-- jvectormap -->
     <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-    <!-- Daterange picker ->
+    <!-- Daterange picker -->
     <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker-bs3.css">
     <!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
@@ -149,13 +149,13 @@ desired effect
                     <div class="">
                         <section class="content">
                             <div class="row">
-                                <?php if($_SESSION["RoleID"] == ROLE_ID_ADMIN){ ?>
+                                <?php if($_SESSION["RoleID"] == ROLE_ID_ADMIN || $_SESSION["RoleID"] == ROLE_ID_PLANT){ ?>
                                     <div class="col-md-6 col-sm-6 col-xs-12">
                                         <div class="info-box">
                                             <span class="info-box-icon bg-yellow"><i class="fa  fa-cubes"></i></span>
                                             <div class="info-box-content">
                                                 <span class="info-box-text">Total Gas Dispatched Today</span>
-                                                <span class="info-box-number"><?php echo @(int)mysql_result(mysql_query("SELECT SUM(CompanyTotalWeight)-SUM(TierWeight) AS TotalGasDispatched FROM purchase_details WHERE DATE_FORMAT(DateAdded, '%Y-%m-%d') >= '" . date('Y-m-01') . "' "), 0); ?>KGs</span>
+                                                <span class="info-box-number"><?php echo @(int)mysql_result(mysql_query("SELECT SUM(CompanyTotalWeight)-SUM(TierWeight) AS TotalGasDispatched FROM purchase_details WHERE DATE_FORMAT(DateAdded, '%Y-%m-%d') >= '" . date('Y-m-d') . "' "), 0); ?>KGs</span>
                                             </div>
                                         </div>
                                     </div>
@@ -164,7 +164,7 @@ desired effect
                                             <span class="info-box-icon bg-blue"><i class="fa fa-star"></i></span>
                                             <div class="info-box-content">
                                                 <span class="info-box-text">Total Gas Savings Today</span>
-                                                <span class="info-box-number"><?php echo @(int)mysql_result(mysql_query("SELECT SUM(Savings) AS Savings FROM cylinder_savings WHERE DATE_FORMAT(DateAdded, '%Y-%m-%d') >= '" . date('Y-m-01') . "' "), 0); ?>KGs</span>
+                                                <span class="info-box-number"><?php echo @(int)mysql_result(mysql_query("SELECT SUM(cs.Savings - ct.Wastage) AS Savings FROM cylinder_savings cs LEFT JOIN cylinders c ON c.ID=cs.CylinderID LEFT JOIN cylindertypes ct ON ct.ID=c.CylinderType WHERE DATE_FORMAT(cs.DateAdded, '%Y-%m-%d') >= '" . date('Y-m-d') . "' " . ($_SESSION["RoleID"] == ROLE_ID_PLANT ? ' AND c.PlantID='.$_SESSION["ID"] : '')), 0); ?>KGs</span>
                                             </div>
                                         </div>
                                     </div>
@@ -205,7 +205,7 @@ desired effect
                                   </div>--->
 
                             <div class="row">
-                                <div class="col-md-8">
+                                <div class="col-md-12">
                                     <div class="box">
                                         <div class="box-header with-border">
                                             <h3 class="box-title">Monthly Sales Report</h3>
@@ -228,7 +228,6 @@ desired effect
                                                     </p>
 
                                                     <div class="chart">
-                                                        <!-- Sales Chart Canvas -->
                                                         <canvas id="salesChart1" style="height: 180px;"></canvas>
                                                     </div>
                                                     <!-- /.chart-responsive -->
@@ -240,21 +239,12 @@ desired effect
                                         <!-- ./box-body -->
                                         <div class="box-footer">
                                             <div class="row">
-                                                <div class="col-md-6 text-center">
+                                                <div class="col-md-12 text-center">
                                                     <div class="description-block border-right">
-                                                        <!--                    <span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 17%</span>-->
-                                                        <h5 class="description-header">Rs. <span id="Revenue">0</span>
-                                                        </h5>
-                                                        <span class="description-text">TOTAL REVENUE</span>
-                                                    </div>
-                                                    <!-- /.description-block -->
-                                                </div>
-                                                <div class="col-md-6 text-center">
-                                                    <div class="description-block border-right">
-                                                        <!--                    <span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 17%</span>-->
+                                                        <!--<span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 17%</span>-->
                                                         <h5 class="description-header">Rs. <span
-                                                                    id="MonthRevenue">0</span></h5>
-                                                        <span class="description-text">THIS MONTH REVENUE</span>
+                                                                    id="MonthRevenue"><?php echo @financials((int)mysql_result(mysql_query("SELECT SUM(Paid + Unpaid) AS TotalSales FROM sales WHERE DATE_FORMAT(DateAdded, '%Y-%m') >= '" . date('Y-m') . "' "), 0)); ?></span></h5>
+                                                        <span class="description-text">THIS MONTH SALE</span>
                                                     </div>
                                                     <!-- /.description-block -->
                                                 </div>
@@ -262,44 +252,6 @@ desired effect
                                             <!-- /.row -->
                                         </div>
                                         <!-- /.box-footer -->
-                                    </div>
-                                    <!-- /.box -->
-                                </div>
-                                <!-- /.col -->
-                                <div class="col-md-4">
-                                    <div class="box">
-                                        <div class="box-header with-border">
-                                            <h3 class="box-title">Orders: <?php echo date('M Y'); ?></h3>
-
-                                            <div class="box-tools pull-right">
-                                                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-                                                            class="fa fa-minus"></i>
-                                                </button>
-                                                <button type="button" class="btn btn-box-tool" data-widget="remove"><i
-                                                            class="fa fa-times"></i></button>
-                                            </div>
-                                        </div>
-                                        <!-- /.box-header -->
-                                        <div class="box-body">
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <div class="chart-responsive">
-                                                        <canvas id="pieChart" height="150"></canvas>
-                                                    </div>
-                                                    <!-- ./chart-responsive -->
-                                                </div>
-                                                <!-- /.col -->
-                                                <div class="col-md-12">
-                                                    <ul class="chart-legend clearfix">
-                                                        <li><i class="fa fa-circle-o text-red"></i>&nbsp;&nbsp;&nbsp;
-                                                            <strong class="pull-right">sa</strong></li>
-                                                    </ul>
-                                                </div>
-                                                <!-- /.col -->
-                                            </div>
-                                            <!-- /.row -->
-                                        </div>
-                                        <!-- /.box-body -->
                                     </div>
                                     <!-- /.box -->
                                 </div>
@@ -341,38 +293,120 @@ desired effect
 </script>
 <!-- Bootstrap 3.3.5 -->
 <script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
+<!-- Morris.js charts -- >
 <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
 <script src="plugins/morris/morris.min.js"></script>
-<!-- Sparkline -->
+<!- - Sparkline -- >
 <script src="plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
+<!- - jvectormap -- >
 <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
+<!- - jQuery Knob Chart -- >
 <script src="plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker ->
+<!- - daterangepicker -- >
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
 <script src="plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
+<!- - Bootstrap WYSIHTML5 -- >
 <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+-->
 <!-- ChartJS 1.0.1 -->
 <script src="plugins/chartjs/Chart.min.js"></script>
-<!-- Slimscroll -->
+<!-- Slimscroll -- >
 <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
+<!- - FastClick -- >
 <script src="plugins/fastclick/fastclick.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
-<!-- Calendar -->
+<!- - Calendar -- >
 <script src="plugins/fullcalendar/fullcalendar.min.js"></script>
+-->
 <!-- AdminLTE App -->
 <script src="dist/js/app.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -- >
 <script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
+<!- - AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
 
 <script>
+
+
+    // Get context with jQuery - using jQuery's .get() method.
+    var salesChartCanvas = $('#salesChart1').get(0).getContext('2d');
+    // This will get the first returned node in the jQuery collection.
+    var salesChart       = new Chart(salesChartCanvas);
+
+    <?php
+    $i = 0;
+    $salesArr = array();
+    $monthsArr = array();
+    $Revenue = 0;
+    for($dCou=11; $dCou>=0; $dCou--) {
+        $dateAdded = date('Y-m', strtotime("-$dCou month"));
+        $salesChart1SQL = mysql_query("SELECT SUM(s.Paid + s.Unpaid) AS Total, '".$dateAdded."' AS month FROM sales s LEFT JOIN users u ON s.PerformedBy=u.ID WHERE DATE_FORMAT(s.DateAdded, '%Y-%m') = '".$dateAdded."' " . ($_SESSION["RoleID"] == ROLE_ID_PLANT ? ' AND u.PlantID="'.$_SESSION["ID"].'"' : ' AND s.PerformedBy="'.$_SESSION["ID"].'"')) or die(mysql_error());
+        $Rs = mysql_fetch_array($salesChart1SQL);
+        array_push($salesArr, financials($Rs["Total"]));
+        array_push($monthsArr, date('F Y', strtotime(date('Y-m-d', strtotime($Rs["month"])))));
+    }
+    ?>
+    var salesChartData = {
+        labels  : ['<?php echo implode("', '", $monthsArr); ?>'],
+        datasets: [
+            {
+                label               : 'Sale Amount',
+                fillColor           : '#ffd101',
+                strokeColor         : '#ffd101',
+                pointColor          : '#3b8bba',
+                pointStrokeColor    : 'rgba(60,141,188,1)',
+                pointHighlightFill  : '#fff',
+                pointHighlightStroke: 'rgba(60,141,188,1)',
+                data                : [<?php echo implode(",", $salesArr); ?>]
+            }
+        ]
+    };
+
+    var salesChartOptions = {
+        // Boolean - If we should show the scale at all
+        showScale               : true,
+        // Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines      : false,
+        // String - Colour of the grid lines
+        scaleGridLineColor      : 'rgba(0,0,0,.05)',
+        // Number - Width of the grid lines
+        scaleGridLineWidth      : 1,
+        // Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+        // Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines  : true,
+        // Boolean - Whether the line is curved between points
+        bezierCurve             : true,
+        // Number - Tension of the bezier curve between points
+        bezierCurveTension      : 0.3,
+        // Boolean - Whether to show a dot for each point
+        pointDot                : false,
+        // Number - Radius of each point dot in pixels
+        pointDotRadius          : 4,
+        // Number - Pixel width of point dot stroke
+        pointDotStrokeWidth     : 1,
+        // Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+        pointHitDetectionRadius : 20,
+        // Boolean - Whether to show a stroke for datasets
+        datasetStroke           : true,
+        // Number - Pixel width of dataset stroke
+        datasetStrokeWidth      : 2,
+        // Boolean - Whether to fill the dataset with a color
+        datasetFill             : true,
+        // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+        maintainAspectRatio     : true,
+        // Boolean - whether to make the chart responsive to window resizing
+        responsive              : true
+    };
+
+    // Create the line chart
+    salesChart.Line(salesChartData, salesChartOptions);
+
+
+
+
+
     $(window).load(function () {
         $(".afterloadreemoveActive").removeClass("active");
     });
